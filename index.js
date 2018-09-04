299cf5532cfc118ded9b48df4569f450632234ef
Srivastava <siddharth.srivastava@sap.com>
2018-09-04 11:36:20 +0400

    bugfix:port required for Heroku

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 require('dotenv').config();
+const http = require('http');
 const Jobs = require('./lib/Jobs');
 
 Jobs.OpenWeatherMap('q=Noida,in').start();
+
+http.createServer((request, response) => {
+	response.writeHead(200, { 'Content-Type': 'text/plain' });
+  	response.end('Server running.');
+}).listen(process.env.PORT);
